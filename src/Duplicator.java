cb134b09eab2d5db9f7962fcef13faf6957d2758
s.hitipeuw <werksimon@gmail.com>
2019-10-28 21:03:18 +0100

    Initial commit

---
+++ src/Duplicator.java
@@ -0,0 +1,34 @@
+public class Duplicator {
+
+    public static void main(String[] arg) {
+
+    }
+
+    /**
+     * TEST DIRECTORY en TEST CLASS
+     *
+     * 1. Maak een test directory aan.
+     * 2. Maak van deze directory een Test Resource Folder wordt
+     *
+     *  3. Maak een test class aan.
+     *
+     */
+
+    /**
+     *
+     * 1. Maak een SUT aan in je test class.
+     *
+     * 2. Maak een test method aan waarbij je de onderstaande
+     *    functie duplicateCounter() test
+     *
+     * 3. Run de test en bekijk het resultaat
+     *
+     * 4. Maak de functie duplicateCounter() af zodat de test wel werkt.
+     *
+     */
+    public int duplicateCounter() {
+
+    }
+
+
+}
